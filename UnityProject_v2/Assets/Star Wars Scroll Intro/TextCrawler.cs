5f83d3781ce5a7e9e46ac80197910437bc48dd6b
Olivier Ngo <olivier.ngo.pro@gmail.com>
2024-12-21 21:53:41 +0100

    Ajout de la scène d'intro
    Ajout de la scène d'introduction du lore à la manière de star wars

---
+++ UnityProject_v2/Assets/Star Wars Scroll Intro/TextCrawler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextCrawler : MonoBehaviour
+{
+    
+    [SerializeField] private float _scrollSpeed = 80f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Camera.main.transform.up * _scrollSpeed * Time.deltaTime);
+    }
+}
